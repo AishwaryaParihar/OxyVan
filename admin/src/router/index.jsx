5b43af294a1bdd0897a758be18573d57082775e6
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-09-24 16:41:06 +0530

    route fix

--- admin/src/router/index.jsx
+++ admin/src/router/index.jsx
@@ -10,12 +10,9 @@ import ContactForm from "../component/ContactForm";
 import UserRecord from "../component/UserRecord";
 import UserRecordForm from "../component/UserRecordForm";
 import VolunteerDetails from "../component/VolunteerDetails";
-<<<<<<< HEAD
 import Dashboard from "../component/Dashboard/Dashboard";
 import TotalTrees from "../component/Dashboard/TotalTrees";
-=======
 import Register from "../component/Register";
->>>>>>> 45ab22a289d9beea6b71c6c4ecca5d294c9b4fdd
 
 
 export const routeradmin = createBrowserRouter([
